9e5bb5fbac0c0a83e2d6b9834122b65ac24fb226
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-01-25 13:42:08 +0100

    Initial New Customer View - basic form

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -30,6 +30,11 @@ namespace Vidly.Controllers
             return View(customers);
         }
 
+        public ActionResult New()
+        {
+            return View();
+        }
+
         public ActionResult Details(int id)
         {
 
