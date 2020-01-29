27b2059d9900a1638a0599635cabd5684e0b0167
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-01-29 22:27:19 +0100

    Adding validation, its styling and data annotation

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Vidly
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
