08cec9ed941a81de0237118b6f5730ed7da4fd53
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-02-01 19:23:12 +0100

    MovieDto, MovieController for Web API

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -14,6 +14,8 @@ namespace Vidly.App_Start
         {
             Mapper.CreateMap<Customer, CustomerDto>();
             Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>();
         }
     }
 }
