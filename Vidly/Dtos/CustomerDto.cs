551bc6fa601bb020e9b101f7b62d0ee1550176e5
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-02-01 17:33:32 +0100

    customerDto -> IHttpActionResult

--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -19,7 +19,7 @@ namespace Vidly.Dtos
 
         public byte MembershipTypeId { get; set; }
 
-        [Min18YearsIfAMember]
+        // [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
 }
