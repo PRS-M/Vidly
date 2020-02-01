5e1bb1a06b5ae3e70e007f6f3133cda913d268b3
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-02-01 17:32:58 +0100

    JSON Formatting

--- Vidly/App_Start/WebApiConfig.cs
+++ Vidly/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -9,6 +11,10 @@ namespace Vidly
     {
         public static void Register(HttpConfiguration config)
         {
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
