821325a6d7204eb3270fd015ca9660d08d1d0fb4
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-02-15 20:46:08 +0100

    Add Rental to Db

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vidly/Models/Rental.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public Customer Customer { get; set; }
+
+        [Required]
+        public Movie Movie { get; set; }
+
+        /*
+        public int CustomerId { get; set; }
+        public List<int> MovieIds { get; set; }
+        */
+
+        public DateTime DateRented { get; set; }
+        public DateTime? DateReturned { get; set; }
+    }
+}
