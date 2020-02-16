6f3634b5cae56e7366d220de32743cb13b958094
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-02-16 19:58:42 +0100

    Add NumberAvailable decreasing

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -5,15 +5,43 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Vidly.Dtos;
+using Vidly.Models;
 
 namespace Vidly.Controllers.Api
 {
     public class NewRentalsController : ApiController
     {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                movie.NumberAvailable--;
+
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now,
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
+
