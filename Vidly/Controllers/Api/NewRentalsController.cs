f07ad100c23dc6bde0c738a1faae05cf8cc85143
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-02-16 20:15:48 +0100

    Optimistic Approach for Edge Cases (reduced amount of statements)

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -21,18 +21,9 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            if (newRental.MovieIds.Count == 0)
-                return BadRequest("No Movie Ids have been given.");
-
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == newRental.CustomerId);
-
-            if (customer == null)
-                return BadRequest("CustomerId is not vaild.");
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
 
             var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
-
-            if (movies.Count != newRental.MovieIds.Count)
-                return BadRequest("One or more MovieIds are invalid.");
 
             foreach (var movie in movies)
             {
